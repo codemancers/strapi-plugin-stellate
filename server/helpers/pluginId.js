85a30f0e7a6cd29171f1a73cdd8310b6dfe5255c
Syed Sibtain <syedsibtain@Syeds-MacBook-Air.local>
2023-04-19 17:51:36 +0530

    fix: config undefined in plugin Id

--- server/helpers/pluginId.js
+++ server/helpers/pluginId.js
@@ -1,6 +1,6 @@
 const pluginPkg = require("../../package.json");
 
-const pluginId = pluginPkg.name.replace(
+const pluginId = pluginPkg.strapi.name.replace(
   /^(@[^-,.][\w,-]+\/|strapi-)plugin-/i,
   ""
 );
